8361c4ee8751b5c0aed91b6bc87498d6e30578a5
Dmitriy <106700401+gupalenok@users.noreply.github.com>
2026-01-21 21:23:37 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+# DaData API
+DADATA_API_KEY = os.getenv("DADATA_API_KEY", "")
+DADATA_SECRET_KEY = os.getenv("DADATA_SECRET_KEY", "")
+
+# Реквизиты вашей компании (исполнитель)
+COMPANY = {
+    "name": "ООО «Чипмедиа.ру»",
+    "name_full": "Общество с ограниченной ответственностью «Чипмедиа.ру»",
+    "inn": "7203406721",
+    "kpp": "720301001",
+    "ogrn": "1167232094677",
+    "address": "625003, г. Тюмень, ул. Володарского, дом 14, кб. 505",
+    "phone": "8(3452) 612-950",
+    "email": "info@cheapmedia.ru",
+    "bank_name": "ЗАПАДНО-СИБИРСКОЕ ОТДЕЛЕНИЕ №8647 ПАО СБЕРБАНК",
+    "bik": "047102651",
+    "account": "40702810267100005911",
+    "corr_account": "30101810800000000651",
+}
+
+# Настройки счёта
+INVOICE_PREFIX = "ЧМ"
+INVOICE_START_NUMBER = 1
+
+# Срок оплаты (дней от даты счёта)
+PAYMENT_DAYS = 3
